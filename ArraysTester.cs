4485502f887dcfee3907a85a91c8573ff22b4e66
chanyoung1999 <93686210+chanyoung1999@users.noreply.github.com>
2023-01-14 22:56:37 -0700

    Create ArraysTester.cs

---
+++ ArraysTester.cs
@@ -0,0 +1,75 @@
+namespace prove_01;
+
+public static class ArraysTester {
+    /// <summary>
+    /// Entry point for the Prove 1 tests
+    /// </summary>
+    public static void Run() {
+        // Sample Test Cases (may not be comprehensive)
+        Console.WriteLine("\n=========== PROBLEM 1 TESTS ===========");
+        double[] multiples = MultiplesOf(7, 5);
+        Console.WriteLine($"<double>{{{string.Join(',', multiples)}}}"); // <double>{7, 14, 21, 28, 35}
+        multiples = MultiplesOf(1.5, 10);
+        Console.WriteLine($"<double>{{{string.Join(',', multiples)}}}"); // <double>{1.5, 3.0, 4.5, 6.0, 7.5, 9.0, 10.5, 12.0, 13.5, 15.0}
+        multiples = MultiplesOf(-2, 10);
+        Console.WriteLine($"<double>{{{string.Join(',', multiples)}}}"); // <double>{-2, -4, -6, -8, -10, -12, -14, -16, -18, -20}
+
+        Console.WriteLine("\n=========== PROBLEM 2 TESTS ===========");
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        RotateListRight(numbers, 1);
+        Console.WriteLine($"<List>{{{string.Join(',', numbers)}}}"); // <List>{9, 1, 2, 3, 4, 5, 6, 7, 8}
+        numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        RotateListRight(numbers, 5);
+        Console.WriteLine($"<List>{{{string.Join(',', numbers)}}}"); // <List>{5, 6, 7, 8, 9, 1, 2, 3, 4}
+        numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        RotateListRight(numbers, 3);
+        Console.WriteLine($"<List>{{{string.Join(',', numbers)}}}"); // <List>{7, 8, 9, 1, 2, 3, 4, 5, 6}
+        numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        RotateListRight(numbers, 9);
+        Console.WriteLine($"<List>{{{string.Join(',', numbers)}}}"); // <List>{1, 2, 3, 4, 5, 6, 7, 8, 9}
+        MultiplesOf(3,5)
+        GetRange(3, 5)
+        console.WriteLine($"<List>{{{string.Join(',', numbers)}}}"); // <List>{5, 6, 7, 8, 9, 1, 2, 3, 4}
+        numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        RemoveRange(index, 5)
+        console.WriteLine($"<List>{{{string.Join(',', numbers)}}}"); // <List>{5, 6, 7, 8, 9, 1, 2, 3, 4}
+        numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        AddRange(list or array)
+        console.WriteLine($"<List>{{{string.Join(',', numbers)}}}"); // <List>{5, 6, 7, 8, 9, 1, 2, 3, 4}
+        numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        InsertRange(index, list or array)
+        console.WriteLine($"<List>{{{string.Join(',', numbers)}}}"); // <List>{5, 6, 7, 8, 9, 1, 2, 3, 4}
+        numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        Add(value)
+        console.WriteLine($"<List>{{{string.Join(',', numbers)}}}"); // <List>{5, 6, 7, 8, 9, 1, 2, 3, 4}
+        numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        Insert(index, value)
+        console.WriteLine($"<List>{{{string.Join(',', numbers)}}}"); // <List>{5, 6, 7, 8, 9, 1, 2, 3, 4}
+        numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        RemoveAt(index)
+        console.WriteLine($"<List>{{{string.Join(',', numbers)}}}"); // <List>{5, 6, 7, 8, 9, 1, 2, 3, 4}
+        numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    }
+    /// <summary>
+    /// This function will produce a list of size 'length' starting with 'number' followed by multiples of 'number'.  For 
+    /// example, MultiplesOf(7, 5) will result in: {7, 14, 21, 28, 35}.  Assume that length is a positive
+    /// integer greater than 0.
+    /// </summary>
+    /// <returns>array of doubles that are the multiples of the supplied number</returns>
+    private static double[] MultiplesOf(double number, int length)
+    {
+        // TODO Problem 1 Start
+        return new double[0];
+    }
+    
+    /// <summary>
+    /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
+    /// &lt;List&gt;{1, 2, 3, 4, 5, 6, 7, 8, 9} and an amount is 5 then the list returned should be 
+    /// &lt;List&gt;{5, 6, 7, 8, 9, 1, 2, 3, 4}.  The value of amount will be in the range of 1 and 
+    /// <code>data.Count</code>.
+    /// </summary>
+    private static void RotateListRight(List<int> data, int amount)
+    {
+        // TODO Problem 2 Start
+    }
+}
